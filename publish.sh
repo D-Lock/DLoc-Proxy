0fcde4e85592cc730c4ef5cebadd0e349539f294
RedbackThomson <redbackthomson@gmail.com>
2017-01-02 21:06:56 +0800

    Added easy tagging script

---
+++ publish.sh
@@ -0,0 +1,8 @@
+# Build with the timestamp as the tag
+docker build -t dloc/dloc-proxy:$TIMESTAMP
+
+# Push the new image to DockerHub
+docker push dloc/dloc-proxy:$TIMESTAMP
+
+# Return the image name
+echo dloc/dloc-proxy:$TIMESTAMP
